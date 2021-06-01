1eb64ea98c37d245748ed4956905c67a56dd2cbe
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2021-06-01 09:49:31 +0200

    fix: install Go always

--- .ci/scripts/install-go.sh
+++ .ci/scripts/install-go.sh
@@ -8,16 +8,16 @@ HOME=${HOME:?$MSG}
 ARCH=$(uname -s| tr '[:upper:]' '[:lower:]')
 GVM_CMD="${HOME}/bin/gvm"
 
-if command -v go
-then
-    echo "Found Go. Checking version.."
-    FOUND_GO_VERSION=$(go version|awk '{print $3}'|sed s/go//)
-    if [ $FOUND_GO_VERSION == $GO_VERSION ]
-    then
-        echo "Versions match. No need to install Go. Exiting."
-        exit 0
-    fi
-fi
+# if command -v go
+# then
+#     echo "Found Go. Checking version.."
+#     FOUND_GO_VERSION=$(go version|awk '{print $3}'|sed s/go//)
+#     if [ $FOUND_GO_VERSION == $GO_VERSION ]
+#     then
+#         echo "Versions match. No need to install Go. Exiting."
+#         exit 0
+#     fi
+# fi
 
 echo "UNMET DEP: Installing Go"
 mkdir -p "${HOME}/bin"
